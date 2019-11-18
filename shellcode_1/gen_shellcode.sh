a830c16c5c91d953279bb645a14786f264ec22cf
impendingsegfault <57508692+impendingsegfault@users.noreply.github.com>
2019-11-18 16:17:03 +1100

    cleanup filenames

--- shellcode_1/gen_shellcode.sh
+++ shellcode_1/gen_shellcode.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sc=$(echo x/11bx evil_function|gdb -q ./shellcode |cut -f2 -d':'|grep 0x|tr -d '\n'|tr '\t' ','|sed -e s%,%%)
+sc=$(echo x/11bx evil_function|gdb -q ./shellcode_file |cut -f2 -d':'|grep 0x|tr -d '\n'|tr '\t' ','|sed -e s%,%%)
 
 sed s%^unsigned.*evil.*$%unsigned\ char\ evil_shellcode[]\ =\ {${sc}}\;% < hello_world.c > hello_world.c.new
 
