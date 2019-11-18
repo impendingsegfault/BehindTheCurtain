1d6f2f69aedc7493f003d530a667f4476f6d292b
impendingsegfault <57508692+impendingsegfault@users.noreply.github.com>
2019-11-18 16:11:26 +1100

    delete shellcode directory

--- shellcode/gen_shellcode.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#sc=$(echo x/11bx evil_function|gdb -q ./shellcode |cut -f2 -d':'|grep 0x|tr -d '\n'|tr '\t' ','|sed -e s%,%%)
-sc=$(echo x/23bx evil_function|gdb -q ./shellcode |cut -f2 -d':'|grep 0x|tr -d '\n'|tr '\t' ','|sed -e s%,%%)
-#sc=$(echo x/44bx evil_function|gdb -q ./shellcode |cut -f2 -d':'|grep 0x|tr -d '\n'|tr '\t' ','|sed -e s%,%%)
-
-sed s%^unsigned.*evil.*$%unsigned\ char\ evil_shellcode[]\ =\ {${sc}}\;% < hello_world.c > hello_world.c.new
-
-mv hello_world.c.new hello_world.c
